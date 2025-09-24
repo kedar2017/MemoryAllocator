ff5ce32dece0dff967a4d732704f6f2687cac584
Kedar Joshi <kedarjoshi7777@gmail.com>
2025-09-23 20:07:24 -0700

    GEMM workload for Linear allocator: (#3)
    - Tiled approach
    - Tiled as MxT for A and TxM for B
    - Removed any alignments for the linear alloc for now
    - handling of tile size which is not multiple of the M size

--- include/linear.h
+++ include/linear.h
@@ -1,32 +1,28 @@
 #include "allocator.h"
 
-inline size_t alignment (void* a) {
-    return (((size_t) a + 63) & ~(63)) - (size_t)a;
-}
-
 class LinearAlloc: Allocator {
 public:
 
     size_t alloc_size_ = 0;
     void* ptr_;
-    size_t offset_ = 0;
     void* base_;
 
     ~LinearAlloc () {free(base_);}
 
     void init (size_t size) {
+        base_ = static_cast<void*> (malloc(size));
         alloc_size_ = size;
-        ptr_ = static_cast<void*> (malloc(size));
-        base_ = ptr_;
+        ptr_ = base_;
     }
 
-    void* allocate (uint32_t size) {
-        size_t adjust = alignment(ptr_);
-        void* curr = ptr_;
-        offset_ = offset_ + adjust + size;
-        if (offset_ > alloc_size_) return nullptr;
-        ptr_ = static_cast<char*>(ptr_) + adjust + size;
-        return reinterpret_cast<void*>((size_t)curr + adjust);
+    void* allocate (size_t size) {
+        size_t curr = (size_t)ptr_;
+        size_t next = curr + size;
+        if (next - (size_t)base_ > alloc_size_) {
+            return nullptr;
+        }
+        ptr_ = (void*)(next);
+        return reinterpret_cast<void*>(curr);
     }
 
     void deallocate () {
--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,14 @@
-#include "../include/linear.h"
+#include "../include/workload/gemm.h"
 #include <cstdio>
 
 int main () {
-    struct TestStruct {
-        int a = 0;
-        int b = 0;
-        int c = 0;
-    };
+    std::vector<uint8_t> A(1024 * 1024, 2);
+    std::vector<uint8_t> B(1024 * 1024, 3);
+    std::vector<uint16_t> C(1024 * 1024);
 
-    LinearAlloc temp;
-    temp.init(100);
-    auto temp_alloc = static_cast<TestStruct*> (temp.allocate(sizeof(TestStruct)));
-    int* temp_alloc_new = static_cast<int*> (temp.allocate(sizeof(int)));
+    uint8_t* A_ptr = reinterpret_cast<uint8_t*>(A.data());
+    uint8_t* B_ptr = reinterpret_cast<uint8_t*>(B.data());
+    uint16_t* C_ptr = reinterpret_cast<uint16_t*>(C.data());
+
+    gemm(A_ptr, B_ptr, C_ptr, 1024, 24);
 }
