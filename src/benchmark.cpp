060fd25f39be82cd373227926d37f248c10a6c1a
kedar2021 <kedar.joshi@plus.ai>
2025-09-30 23:11:13 -0700

    fixed size 100k multiplies gemm wall time comparison: - matrix fixed at 32x32 - duration measured for 100K times multiply of matrices - result stored in vectors for malloc and linear alloc - sweep across tile sizes for comparison

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,35 +1,55 @@
 #include "../include/workload/gemm.h"
 #include <cstdio>
 #include <chrono>
+#include <array>
 
 using clk = std::chrono::steady_clock;
 
-void wall_time_compare_gemm () {
-    std::vector<uint8_t> A(550 * 550, 2);
-    std::vector<uint8_t> B(550 * 550, 3);
+void wall_time_compare_large_num_mult (size_t M_len, size_t tile_size, size_t align_len, std::vector<double>& malloc_ms, std::vector<double>& lin_alloc_ms) {
+
+    std::vector<uint8_t> A(M_len * M_len, 2);
+    std::vector<uint8_t> B(M_len * M_len, 3);
     uint8_t* A_ptr = reinterpret_cast<uint8_t*>(A.data());
     uint8_t* B_ptr = reinterpret_cast<uint8_t*>(B.data());
 
     auto t2 = clk::now();
-    std::vector<uint16_t> C_malloc(550 * 550);
+    std::vector<uint16_t> C_malloc(M_len * M_len);
     uint16_t* C_malloc_ptr = reinterpret_cast<uint16_t*>(C_malloc.data());
-    gemm_malloc(A_ptr, B_ptr, C_malloc_ptr, 550, 4);
+
+    for (int i = 0; i < 100000; i++) {
+        gemm_malloc(A_ptr, B_ptr, C_malloc_ptr, M_len, tile_size);
+    }
+
     auto t3 = clk::now();
 
     double ms_malloc = std::chrono::duration<double, std::milli>(t3 - t2).count();
-    std::cout << "Duration in ms (malloc): " << ms_malloc << " \n";
+    std::cout << "Duration in ms (malloc): " << ms_malloc << " for tile size " << tile_size <<" \n";
+    malloc_ms.push_back(ms_malloc);
 
     auto t4 = clk::now();
-
-    std::vector<uint16_t> C_lin_alloc_aligned(555 * 555);
+    std::vector<uint16_t> C_lin_alloc_aligned(M_len * M_len);
     uint16_t* C_lin_alloc_aligned_ptr = reinterpret_cast<uint16_t*>(C_lin_alloc_aligned.data());
-    gemm_linear_alloc_aligned(A_ptr, B_ptr, C_lin_alloc_aligned_ptr, 555, 8, 128);
+
+    for (int i = 0; i < 100000; i++) {
+        gemm_linear_alloc_aligned(A_ptr, B_ptr, C_lin_alloc_aligned_ptr, M_len, tile_size, align_len);
+    }
 
     auto t5 = clk::now();
     double ms_lin_alloc_aligned = std::chrono::duration<double, std::milli>(t5 - t4).count();
-    std::cout << "Duration in ms (linear alloc aligned): " << ms_lin_alloc_aligned << " \n";
+    std::cout << "Duration in ms (linear alloc aligned): " << ms_lin_alloc_aligned << " for tile size " << tile_size  << " \n";
+    lin_alloc_ms.push_back(ms_lin_alloc_aligned);
 }
 
 int main () {
-    wall_time_compare_gemm();
+    /*
+    Fixed matrix size of 32x32 
+    sweep across tile sizes in *tile_size_sweep*
+    comparison between the wall times for 100K multiplies of 32x32 matrices
+    */
+    std::array<size_t, 4> tile_size_sweep = {2, 4, 8, 16};
+    std::vector<double> gemm_malloc_duration_ms;
+    std::vector<double> gemm_linear_alloc_duration_ms;
+    for (int i = 0; i < tile_size_sweep.size(); i++) {
+        wall_time_compare_large_num_mult(32, tile_size_sweep[i], 64, gemm_malloc_duration_ms, gemm_linear_alloc_duration_ms);
+    }
 }
